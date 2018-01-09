6c76172836efce6f82ede0b2158631c5103546d2
Sam Chai <sam.chai0501@gmail.com>
2018-01-09 17:55:48 -0500

    formatting to display all lesson information

--- client/components/lessons.js
+++ client/components/lessons.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 const Lessons = (props) => {
   const { lessons } = props;
   return (
     <div>
       <ul>
-        { lessons.map(lesson => {
-          return (<li key={lesson.id}>{lesson.description}</li>)
+        {lessons.map(lesson => {
+          return (
+            <div key={lesson.id}>
+              <h2>{lesson.name}</h2>
+              <li>Category: {lesson.category}</li>
+              <li>Description: {lesson.description}</li>
+              <li>Image: {lesson.imageUrl}</li>
+              <li>Price: {lesson.price}</li>
+            </div>
+          )
         })}
       </ul>
     </div>
