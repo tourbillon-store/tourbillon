ce1c432ea60d48a48474b70d5ffffe13a529eb61
Sam Chai <sam.chai0501@gmail.com>
2018-01-10 18:04:15 -0500

    add firstName and lastName fields to the user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,6 +3,18 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
