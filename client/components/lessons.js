669ca3cf27e99a5b66dfe0d9d8f36bb1bebd65dc
Josh Luria <josh.luria@gmail.com>
2018-01-10 16:46:44 -0500

    Remove extraneous div from lessons component

--- client/components/lessons.js
+++ client/components/lessons.js
@@ -5,22 +5,20 @@ import {withRouter, Link} from 'react-router-dom'
 const Lessons = (props) => {
   const { lessons } = props;
   return (
-    <div>
-      <ul>
-        {lessons.map(lesson => {
-          return (
-            <div key={lesson.id}>
-              <Link to={`/lessons/${lesson.id}`}><h2>{lesson.name}</h2></Link>
-              <li>User: {lesson.userId}</li>
-              <li>Category: {lesson.category}</li>
-              <li>Description: {lesson.description}</li>
-              <li>Image: {lesson.imageUrl}</li>
-              <li>Price: {lesson.price}</li>
-            </div>
-          )
-        })}
-      </ul>
-    </div>
+    lessons.map(lesson => {
+      return (
+        <div key={lesson.id}>
+          <Link to={`/lessons/${lesson.id}`}><h2>{lesson.name}</h2></Link>
+          <ul>
+            <li>User: {lesson.userId}</li>
+            <li>Category: {lesson.category}</li>
+            <li>Description: {lesson.description}</li>
+            <li>Image: {lesson.imageUrl}</li>
+            <li>Price: {lesson.price}</li>
+          </ul>
+        </div>
+      )
+    })
   )
 }
 
