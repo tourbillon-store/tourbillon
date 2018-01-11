4989279e5799bd05787833e2bd87297ff4120ab5
Sam Chai <sam.chai0501@gmail.com>
2018-01-11 11:30:13 -0500

    spacing

--- client/components/AllWatches.js
+++ client/components/AllWatches.js
@@ -39,7 +39,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     resetWatch() {
-      dispatch(resetWatch({loading: true}))
+      dispatch(resetWatch({ loading: true }))
     }
   }
 }
