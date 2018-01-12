b854eeed81cad40b6c634aeea0f176ecdd49b88c
allenhj <allendotjohnson@gmail.com>
2018-01-12 12:06:41 -0500

    Addressing PR comments pt 2

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -4,7 +4,7 @@ const db = require('../db')
 const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM,
-    values: ['cart', 'created', 'processing', 'cancelled', 'completed']
+    values: ['cart', 'created', 'processing', 'cancelled', 'completed'],
   }
 })
 
--- server/db/models/orderWatch.js
+++ server/db/models/orderWatch.js
@@ -2,8 +2,20 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const OrderWatch = db.define('order_watch', {
-  fixedPrice: Sequelize.INTEGER,
-  quantity: Sequelize.INTEGER
+  fixedPrice: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1
+    }
+  }
 })
 
 module.exports = OrderWatch
