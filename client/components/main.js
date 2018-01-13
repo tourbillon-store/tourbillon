0bbf32e106f69cbe127e571198c5e6802693910b
Sam Chai <sam.chai0501@gmail.com>
2018-01-13 16:41:39 -0500

    only display admin link if user is admin

--- client/components/main.js
+++ client/components/main.js
@@ -11,7 +11,7 @@ import { logout } from '../store'
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  const { children, handleClick, isLoggedIn } = props
+  const { children, handleClick, isLoggedIn, isAdmin } = props
   return (
     <div>
       <h1>GRACE SHOPPER</h1>
@@ -22,7 +22,7 @@ const Main = (props) => {
               {/* The navbar will show these links after you log in */}
               <Link to="/home">Home</Link>
               <Link to="/watches">Watches</Link>
-              <Link to="/admin">Admin</Link>
+              {isAdmin && <Link to="/admin">Admin</Link>}
               <Link to="/cart">Cart</Link>
               <a href="#" onClick={handleClick}>Logout</a>
             </div>
@@ -47,7 +47,8 @@ const Main = (props) => {
  */
 const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    isAdmin: !!state.user.isAdmin
   }
 }
 
