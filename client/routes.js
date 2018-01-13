a386bf26d8caab05e301d00f3bf4bbd2d16c7fdd
Sam Chai <sam.chai0501@gmail.com>
2018-01-12 22:34:41 -0500

    rename admin and remove orders from import

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import { Route, Switch, Router } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
 import { Main, Login, Signup, UserHome, VisitorHome, UserCart, VisitorCart, AllWatches, SingleWatch } from './components'
-import { AdminMain, Orders } from './admin'
+import { AdminMain } from './admin'
 import { me, fetchWatches } from './store'
 
 /**
