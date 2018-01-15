71e2cb77491ab5ce7c77c766d191796dc7c47710
Josh Luria <josh.luria@gmail.com>
2018-01-15 16:36:53 -0500

    Add a button for returning from the admin panel

--- client/admin/AdminHeader.js
+++ client/admin/AdminHeader.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Header, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import { logout } from '../store'
 
 const AdminHeader = (props) => (
   <Header attached="top" as="h1" block>
     ADMIN
-      <Button floated="right" negative onClick={props.handleClick}>
-      Logout
-    </Button>
+    <Button.Group floated="right">
+      <Button positive>
+        <Link className="view-as-user" to="/watches">
+          View As User
+        </Link>
+      </Button>
+      <Button.Or />
+      <Button negative onClick={props.handleClick}>
+        Logout
+      </Button>
+    </Button.Group>
   </Header>
 )
 
