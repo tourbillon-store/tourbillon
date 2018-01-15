921fc55c9accfde30f4179821b3debca64538844
Sam Chai <sam.chai0501@gmail.com>
2018-01-14 22:36:24 -0500

    update comment

--- client/admin/OrdersTableRow.js
+++ client/admin/OrdersTableRow.js
@@ -6,8 +6,8 @@ const OrdersTableRow = (props) => {
   let key = 0;
   return (
     /*
-    need to return a single jsx element work-around. Could use react fragments
-    but still in beta. Using an array requires a key on every element.
+    Returning a single jsx element work-around. Could use react fragments
+    but Babel support still in beta. Using an array requires a key on every element.
     */
     [
     <Table.Cell key={key++} textAlign="center">
