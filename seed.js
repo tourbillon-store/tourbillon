8b191d34e82d25c8c8a74609c21cacab4f11d8ba
Sam Chai <sam.chai0501@gmail.com>
2018-01-15 11:03:52 -0500

    add chance and reviews seed

--- seed.js
+++ seed.js
@@ -1,3 +1,4 @@
+const chance = require('chance')(123);
 const db = require('./server/db')
 const { User, Watch, Review, Order, OrderWatch } = require('./server/db/models/')
 
@@ -10,11 +11,11 @@ const watches = []
 for (let i = 0; i < makes.length; i++) {
   let make = makes[i]
   let model = models[i]
-  let complication = complications[Math.floor(Math.random() * complications.length)]
-  let maxYear = 2018, minYear = 2000
-  let year = Math.floor(Math.random() * (maxYear - minYear)) + minYear
-  let maxPrice = 1500000, minPrice = 500000
-  let price = Math.floor(Math.random() * (maxPrice - minPrice)) + minPrice
+  let complication = chance.pickone(complications)
+  let minYear = 2000, maxYear = 2018
+  let year = chance.integer({min: minYear, max: maxYear})
+  let minPrice = 500000, maxPrice = 1500000
+  let price = chance.integer({min: minPrice, max: maxPrice})
   watches.push({
     make,
     model,
@@ -44,10 +45,25 @@ const statuses = ['cart', 'created', 'processing', 'cancelled', 'completed']
 
 const numOrders = 20
 for (let i = 0; i < numOrders; i++) {
-  let status = statuses[Math.floor(Math.random() * statuses.length)]
-  let userId = Math.floor(Math.random() * users.length)
+  let status = chance.pickone(statuses)
+  let userId = chance.integer({min: 0, max: users.length - 1})
   orders.push({
     status,
     userId
+  })
+}
+
+const reviews = []
+const numReviews = 50
+for (let i = 0;i < numReviews; i++) {
+  let watchId = chance.integer({min: 0, max: watches.length - 1})
+  let userId = chance.integer({min: 0, max: users.length - 1})
+  let rating = chance.integer({min: 0, max: 5})
+  let content = chance.paragraph()
+  reviews.push({
+    watchId,
+    userId,
+    rating,
+    content
   })
 }
