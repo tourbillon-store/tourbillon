a4c2d436e9f88ab24400d1232acfae4cc166f6c1
Josh Luria <josh.luria@gmail.com>
2018-01-15 15:13:22 -0500

    Remove extra column from Watch Table row

--- client/Admin/AllWatchesTableRow.js
+++ client/Admin/AllWatchesTableRow.js
@@ -32,28 +32,13 @@ const AllWatchesTableRow = (props) => {
       {watch.price}
     </Cell>,
     <Cell key={key++} textAlign="center">
-      {watch.available}
+      {watch.available ? "Available" : "Unavailable"}
     </Cell>,
     <Cell key={key++} textAlign="center">
       {watch.createdAt}
     </Cell>,
     <Cell key={key++} textAlign="center">
       {watch.updatedAt}
-    </Cell>,
-    <Cell key={key++} >
-    {/*
-      <List selection divided verticalAlign="middle">
-        {order.watches.map((watch) => (
-          <List.Item key={watch.id}>
-            <List.Content>
-              <List.Header>ID: {watch.id}</List.Header>
-              Price: {watch.order_watch.quantity}<br />
-              Quantity: {watch.order_watch.fixedPrice}
-            </List.Content>
-          </List.Item>
-        ))}
-      </List>
-     */}
     </Cell>
     ]
   )
