a537b71bc213c6fb290b09a5fe801ef1466d4dd7
Sam Chai <sam.chai0501@gmail.com>
2018-01-14 23:44:21 -0500

    styling to appear larger

--- client/admin/SingleOrder.js
+++ client/admin/SingleOrder.js
@@ -4,22 +4,22 @@ import { Table, List } from 'semantic-ui-react'
 const SingleOrder = (props) => {
   const { order } = props
   return (
-    <Table selectable celled padded attached="bottom">
+    <Table selectable definition size="large" celled padded attached="bottom">
       <Table.Body >
         <Table.Row>
-          <Table.Cell textAlign="center"><p>Status</p></Table.Cell>
+          <Table.Cell textAlign="center">Status</Table.Cell>
           <Table.Cell>{order.status}</Table.Cell>
         </Table.Row>
         <Table.Row>
-          <Table.Cell textAlign="center"><p>CreatedAt</p></Table.Cell>
+          <Table.Cell textAlign="center">CreatedAt</Table.Cell>
           <Table.Cell>{order.createdAt}</Table.Cell>
         </Table.Row>
         <Table.Row>
-          <Table.Cell textAlign="center"><p>UpdatedAt</p></Table.Cell>
+          <Table.Cell textAlign="center">UpdatedAt</Table.Cell>
           <Table.Cell>{order.updatedAt}</Table.Cell>
         </Table.Row>
         <Table.Row>
-          <Table.Cell textAlign="center"><p>Watches</p></Table.Cell>
+          <Table.Cell textAlign="center">Watches</Table.Cell>
           <Table.Cell >
             <List selection divided verticalAlign="middle">
               {order.watches.map((watch) => (
