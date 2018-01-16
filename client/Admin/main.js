cd87e15efd164b724c8e1dd9d318a7d4e691a82c
Sam Chai <sam.chai0501@gmail.com>
2018-01-16 10:39:04 -0500

    refactor to load initial data for admin

--- client/Admin/main.js
+++ client/Admin/main.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { AdminHeader, AdminMenu, Orders, Watches } from './index'
 import { fetchOrders, fetchUsers } from '../store'
 import { connect } from 'react-redux'
 
-const Main = () => {
-  return (
-    <div>
-      <AdminHeader />
-      <div className="admin">
-        <AdminMenu />
-        <Switch>
-          <Route path="/admin/watches" component={Watches} />
-          <Route path="/admin/orders" component={Orders} />
-        </Switch>
+class Main extends Component {
+  componentDidMount() {
+    this.props.loadInitialData()
+  }
+
+  /* eslint-disable class-methods-use-this*/
+  render() {
+    return (
+      <div>
+        <AdminHeader />
+        <div className="admin">
+          <AdminMenu />
+          <Switch>
+            <Route path="/admin/watches" component={Watches} />
+            <Route path="/admin/orders" component={Orders} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
