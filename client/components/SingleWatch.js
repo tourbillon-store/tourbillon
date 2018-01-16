4266be6a8ff35c3c6df7fb414b8eae97a8b368c4
Sam Chai <sam.chai0501@gmail.com>
2018-01-16 17:39:35 -0500

    gs

--- client/components/SingleWatch.js
+++ client/components/SingleWatch.js
@@ -3,35 +3,39 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { fetchWatch, pushWatchToCart } from '../store'
 
+const SingleWatch = (props) => {
+  const {watch, addWatchToCart, user} = props;
+  const unavailableMessage = watch.available ? '' : 'Currently Unavailable'
+  return (
+    !watch.loading && <div>
+      <img src={watch.imageUrl} />
+      <h2>{watch.make} {watch.model}</h2>
+      <h3 className="unavailable-watch">{unavailableMessage}</h3>
+      <ul>
+        <li>Complications: {watch.complications}</li>
+        <li>Year: {watch.year}</li>
+        <li>Price: {watch.price}</li>
+      </ul>
+      {watch.available &&
+        <button onClick={() => addWatchToCart({
+          id: watch.id,
+          make: watch.make,
+          model: watch.model,
+          price: watch.price,
+          createdAt: watch.createdAt
+        }, user.id )}>Add to Cart</button>
+      }
+    </div>
+  )
+}
+
 class Watch extends Component {
   componentDidMount() {
     this.props.getWatch(+this.props.match.params.watchId);
   }
 
   render() {
-    const {watch, addWatchToCart, user} = this.props;
-    const unavailableMessage = watch.available ? '' : 'Currently Unavailable'
-    return (
-      !watch.loading && <div>
-        <img src={watch.imageUrl} />
-        <h2>{watch.make} {watch.model}</h2>
-        <h3 className='unavailable-watch'>{unavailableMessage}</h3>
-        <ul>
-          <li>Complications: {watch.complications}</li>
-          <li>Year: {watch.year}</li>
-          <li>Price: {watch.price}</li>
-        </ul>
-        {watch.available &&
-          <button onClick={() => addWatchToCart({
-            id: watch.id,
-            make: watch.make,
-            model: watch.model,
-            price: watch.price,
-            createdAt: watch.createdAt
-          }, user.id )}>Add to Cart</button>
-        }
-      </div>
-    )
+    return <SingleWatch />
   }
 }
 
