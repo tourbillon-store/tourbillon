0fda2dccadbb8cc2c1016fae3b215fe376af68af
Josh Luria <josh.luria@gmail.com>
2018-01-16 12:13:40 -0500

    Start making a delete button component

---
+++ client/Admin/DeleteButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
+default class DeleteButton extends Component {
+
+}
