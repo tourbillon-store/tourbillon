2bf2f145ee240a5c319f24ccf9cba1178f7bd46c
Sam Chai <sam.chai0501@gmail.com>
2018-01-16 17:40:28 -0500

    undo commit

--- client/components/SingleWatch.js
+++ client/components/SingleWatch.js
@@ -3,39 +3,29 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { fetchWatch, pushWatchToCart } from '../store'
 
-const SingleWatch = (props) => {
-  const {watch, addWatchToCart, user} = props;
-  const unavailableMessage = watch.available ? '' : 'Currently Unavailable'
-  return (
-    !watch.loading && <div>
-      <img src={watch.imageUrl} />
-      <h2>{watch.make} {watch.model}</h2>
-      <h3 className="unavailable-watch">{unavailableMessage}</h3>
-      <ul>
-        <li>Complications: {watch.complications}</li>
-        <li>Year: {watch.year}</li>
-        <li>Price: {watch.price}</li>
-      </ul>
-      {watch.available &&
-        <button onClick={() => addWatchToCart({
-          id: watch.id,
-          make: watch.make,
-          model: watch.model,
-          price: watch.price,
-          createdAt: watch.createdAt
-        }, user.id )}>Add to Cart</button>
-      }
-    </div>
-  )
-}
-
 class Watch extends Component {
   componentDidMount() {
     this.props.getWatch(+this.props.match.params.watchId);
   }
 
   render() {
-    return <SingleWatch />
+    const {watch, addWatchToCart, user} = this.props;
+    const unavailableMessage = watch.available ? '' : 'Currently Unavailable'
+    return (
+      !watch.loading && <div>
+        <img src={watch.imageUrl} />
+        <h2>{watch.make} {watch.model}</h2>
+        <h3 className='unavailable-watch'>{unavailableMessage}</h3>
+        <ul>
+          <li>Complications: {watch.complications}</li>
+          <li>Year: {watch.year}</li>
+          <li>Price: {watch.price}</li>
+        </ul>
+        {watch.available &&
+          <button onClick={() => addWatchToCart(watch.id, user.id )}>Add to Cart</button>
+        }
+      </div>
+    )
   }
 }
 
@@ -43,11 +33,11 @@ const mapStateToProps = ({watch, user}) => ({ watch,  user })
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getWatch(watchId) {
+    getWatch(watchId, userId ) {
       dispatch(fetchWatch(watchId))
     },
-    addWatchToCart(watch, userId) {
-      dispatch(pushWatchToCart(watch, userId))
+    addWatchToCart(watchId, userId) {
+      dispatch(pushWatchToCart(watchId, userId))
     }
   }
 }
