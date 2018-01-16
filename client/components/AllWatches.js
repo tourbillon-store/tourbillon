966e93a115e129a8c9b77520e9889ef25d9b6aa4
Jemyishai <jesse.moskowitz@gmail.com>
2018-01-16 16:20:05 -0500

    card update

--- client/components/AllWatches.js
+++ client/components/AllWatches.js
@@ -2,35 +2,110 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 import { resetWatch } from '../store'
+import { Input, Card, Image, CardContent as content, CardHeader as header, CardDescription as Cdesc, Radio} from 'semantic-ui-react'
 
 class AllWatches extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      make: '',
+      model: '',
+      year: '',
+      price: '',
+      complications: '',
+      avialable: ''
+    };
+
+  this.filterWatch = this.filterWatch.bind(this);
+  this.renderWatchSearch = this.renderWatchSearch.bind(this);
+
+  }
+
   componentDidMount() {
     this.props.resetWatch()
   }
 
   render() {
-    const { watches } = this.props;
+    const { watches } = this.props
     return (
-      <div>
-        {watches.map(watch => {
-          if (watch.available) {
-            return (
-              <div key={watch.id}>
-                <Link to={`/watches/${watch.id}`}><h2>{watch.make} {watch.model}</h2></Link>
-                <ul>
-                  <li>Year: {watch.year}</li>
-                  <li>Complications: {watch.complications}</li>
-                  <li>Image: {watch.imageUrl}</li>
-                  <li>Price: {watch.price}</li>
-                </ul>
-              </div>
-            )
-          }
-        })}
+      <div className="all-watches">
+        <div>
+         {this.renderWatchSearch()}
+       </div>
+        <div>
+          {watches.filter(this.filterWatch).map(watch => {
+            if (watch.available){
+              return (
+                <div key={watch.id}>
+                <Link to={`/watches/${watch.id}`}>
+                <Card >
+                <h2 className="all-watch-card-title">{watch.make} {watch.model}</h2>
+                  <Image src={watch.imageUrl} />
+                  <content className="all-watch-card">
+                    <header>Make: {watch.make}</header>
+                    <header>Model: {watch.model}</header>
+                    <p> Year: {watch.year} </p>
+                    <p>Complications: {watch.complications}</p>
+                    <Cdesc>Price: {watch.price}</Cdesc>
+                  </content>
+                 </Card>
+                 </Link>
+                </div>
+              )
+            }
+          })}
+        </div>
       </div>
     )
   }
+
+
+renderWatchSearch () {
+ return (
+   <div className="all-watch-form">
+      <Input
+      type="text"
+      focus placeholder="MAKE"
+      className=""
+      onChange={evt => this.setState({ make: evt.target.value })}
+    />
+    <Input
+    type="text"
+    focus placeholder="MODEL"
+    className=""
+    onChange={evt => this.setState({ model: evt.target.value })}
+      />
+    <Input
+      type="text"
+      focus placeholder="YEAR"
+      className=""
+      onChange={evt => this.setState({ year: evt.target.value })}
+    />
+    <Input
+      type="text"
+      focus placeholder="COMPLICATIONS"
+      className=""
+      onChange={evt => this.setState({ complications: evt.target.value })}
+      />
+      <Radio label="Available" defaultChecked />
+    </div>
+    )
+  }
+
+  filterWatch (watch) {
+    const makeMatch = new RegExp(this.state.make, 'i');
+    const modelMatch = new RegExp(this.state.model, 'i');
+    const yearMatch  = new RegExp(this.state.year, 'i');
+    const complicationsMatch = new RegExp(this.state.complications, 'i');
+
+    return yearMatch.test(watch.year)
+        && makeMatch.test(watch.make)
+        && modelMatch.test(watch.model)
+        && complicationsMatch.test(watch.complications)
+  }
 }
+
 
 const mapStateToProps = (state) => {
   return {
