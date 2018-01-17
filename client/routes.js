aa0e7538b5f2d92a7058bc643c39d1d1e5552249
Josh Luria <josh.luria@gmail.com>
2018-01-16 23:50:55 -0500

    Resolve merge conflicts

--- client/routes.js
+++ client/routes.js
@@ -4,14 +4,8 @@ import { Route, Switch, Router } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
 import { Main, Login, Signup, UserHome, VisitorHome, Cart, Checkout, AllWatches, SingleWatch, AllOrders, SingleOrder } from './components'
-<<<<<<< HEAD
 import { AdminMain } from './Admin'
 import { me, fetchWatches } from './store'
-=======
-import { AdminMain } from './admin'
-import { me, fetchWatches} from './store'
-
->>>>>>> 91d1b1002be7083f597fa205c2a7290f5ad53151
 
 /**
  * COMPONENT
