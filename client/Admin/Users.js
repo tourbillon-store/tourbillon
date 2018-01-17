a3693b2a96ac6fcff302c25f77a99f8553cc347c
Sam Chai <sam.chai0501@gmail.com>
2018-01-17 03:31:52 -0500

    fix admin fetch users

--- client/Admin/Users.js
+++ client/Admin/Users.js
@@ -1,55 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Table, TableHeader as Header, TableRow as Row, TableHeaderCell as HeaderCell, TableBody as Body, Segment } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { UsersModal } from './index'
-import { adminFetchUsers } from '../store'
 
-class Users extends Component {
-  componentDidMount() {
-    this.props.getUsers()
-  }
-
-  render() {
-    const users = this.props.users
-    return (
-      <Segment attached="bottom">
-        <Table selectable celled padded attached="bottom">
-          <Header>
-            <Row>
-              <HeaderCell textAlign="center" width={2}>User Id</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>User Name</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>Email</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>Google Id</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>Facebook Id</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>Admin Status</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>CreatedAt</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>UpdatedAt</HeaderCell>
-              <HeaderCell textAlign="center" width={2}>Actions</HeaderCell>
-            </Row>
-          </Header>
-          <Body>
-            {users.length ? users.map(user =>
-              <UsersModal key={user.id} user={user} />
-            ) : null}
-          </Body>
-        </Table>
-      </Segment>
-    )}
+const Users = (props) => {
+  const users = props.users
+  return (
+    <Segment attached="bottom">
+      <Table selectable celled padded attached="bottom">
+        <Header>
+          <Row>
+            <HeaderCell textAlign="center" width={2}>User Id</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>User Name</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>Email</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>Google Id</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>Facebook Id</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>Admin Status</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>CreatedAt</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>UpdatedAt</HeaderCell>
+            <HeaderCell textAlign="center" width={2}>Actions</HeaderCell>
+          </Row>
+        </Header>
+        <Body>
+          {users.length ? users.map(user =>
+            <UsersModal key={user.id} user={user} />
+          ) : null}
+        </Body>
+      </Table>
+    </Segment>
+  )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    users: state.users
-  }
-}
+const mapStateToProps = ({users}) => ({users})
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getUsers() {
-      dispatch(adminFetchUsers())
-    }
-  }
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Users))
+export default withRouter(connect(mapStateToProps)(Users))
